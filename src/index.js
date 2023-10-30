6737d9486bbedff0f97d3a6e19bb2b259910429b
imyakin <myakinalex@yandex.ru>
2023-10-30 16:08:46 +0400

    webpack styles initial

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles.scss';
+import './woof.css';
+
+const headline = "welcome to the webpack"
+document.querySelector('h1').innerText = headline;
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development",
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader", "postcss-loader"]
+            }
+        ]
+    },
+
+    devtool: "source-map",
+}
